d0521dd67e1db95c5887ab96693dca361d1c9ef8
EinsVision <einsvision@gmail.com>
2021-02-23 12:47:57 +0900

    first commit

---
+++ slack-clone/src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBL6-2mSL1QyJi8D9b3QpxPK2yw7BzBYD0",
+  authDomain: "slack-clone-141f5.firebaseapp.com",
+  projectId: "slack-clone-141f5",
+  storageBucket: "slack-clone-141f5.appspot.com",
+  messagingSenderId: "998305076166",
+  appId: "1:998305076166:web:629ce9f1a135cec56b18c7"
+};
+
