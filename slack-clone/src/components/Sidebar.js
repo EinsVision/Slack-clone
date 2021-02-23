639d5c63d66ef348ab3a16cf50b6f319b766d879
EinsVision <einsvision@gmail.com>
2021-02-23 16:13:14 +0900

    sidebar (header) building

--- slack-clone/src/components/Sidebar.js
+++ slack-clone/src/components/Sidebar.js
@@ -1,11 +1,40 @@
 import React from 'react'
+import styled from 'styled-components';
+import CreateIcon from '@material-ui/icons/Create';
+import FiberManualRecordIcon from '@material-ui/icons/FiberManualRecord';
 
 function Sidebar() {
   return (
-    <div>
-      
-    </div>
+    <SidebarContainer>
+      <SidebarHeader>
+        <SidebarInfo>
+          <h2>Hodny Coding Fam HQ</h2>
+          <h3>
+            <FiberManualRecordIcon/>
+            Einsvision
+          </h3>
+        </SidebarInfo>
+        <CreateIcon />
+      </SidebarHeader>
+    </SidebarContainer>
   )
 }
 
-export default Sidebar
+export default Sidebar;
+
+const SidebarContainer = styled.div`
+  color: white;
+  background-color: var(--slack-color);
+  flex: 0.3;
+  border-top: 1px solid #49274b;
+  max-width:260px;
+  margin-top: 60px;
+`;
+
+const SidebarHeader = styled.div`
+
+`;
+
+const SidebarInfo = styled.div`
+
+`;
