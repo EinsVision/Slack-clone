9dc0375829a7e6ff0f6127d508db7ea9452df549
EinsVision <einsvision@gmail.com>
2021-02-23 14:37:44 +0900

    applied Styled Components

--- slack-clone/src/Header.js
+++ slack-clone/src/Header.js
@@ -1,12 +1,27 @@
 import React from 'react';
-import './Header.css'
+import './Header.css';
+import styled from 'styled-components';
 
 function Header() {
   return (
-    <div className='header'>
-      
-    </div>
+    <Wrapper>
+      <HeaderContainer>
+        Styled Components
+      </HeaderContainer>
+    </Wrapper>
   )
 }
 
-export default Header
+export default Header;
+
+const HeaderContainer = styled.h1`
+  font-size: 1.5em;
+  text-align: center;
+  color: palevioletred;
+`;
+
+const Wrapper = styled.section`
+  padding: 4em;
+  background: papayawhip;
+`;
+
