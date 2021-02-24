0fbc0863d39e46d4be5e80f2ec9407a24e7d03f5
EinsVision <einsvision@gmail.com>
2021-02-24 16:33:11 +0900

    npm install react-spinkit --save

--- slack-clone/src/App.js
+++ slack-clone/src/App.js
@@ -12,11 +12,24 @@ import Chat from './components/Chat';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import {auth} from './firebase';
 import Login from './components/Login';
+import Spinner from 'react-spinkit';
 
 function App() {
 
   const [user, loading] = useAuthState(auth);
 
+  if(loading){
+    return (
+      <AppLoading>
+        <AppLoadingContents>
+          <img
+            src='https://cdn.mos.cms.futurecdn.net/SDDw7CnuoUGax6x9mTo7dd.jpg'
+            alt='' 
+          />
+        </AppLoadingContents>
+      </AppLoading>
+    )
+  }
   return (
     <div className="app">
       <Router>
@@ -41,6 +54,14 @@ function App() {
 }
 
 export default App;
+
+const AppLoading = styled.div`
+
+`;
+
+const AppLoadingContents = styled.div`
+
+`;
 
 const AppBody = styled.div`
   display: flex;
