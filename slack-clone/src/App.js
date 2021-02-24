34de3f1a8eff91b53b4b0e4c087d8be616c5c0d2
EinsVision <einsvision@gmail.com>
2021-02-24 16:44:36 +0900

    loading function built

--- slack-clone/src/App.js
+++ slack-clone/src/App.js
@@ -26,6 +26,13 @@ function App() {
             src='https://cdn.mos.cms.futurecdn.net/SDDw7CnuoUGax6x9mTo7dd.jpg'
             alt='' 
           />
+
+          <Spinner
+            name='ball-spin-fade-loader'
+            color='purple'
+            fadeIn='none'
+          />
+
         </AppLoadingContents>
       </AppLoading>
     )
@@ -56,11 +63,25 @@ function App() {
 export default App;
 
 const AppLoading = styled.div`
-
+  display: grid;
+  place-items: center;
+  height: 100vh;
+  width: 100%;
 `;
 
 const AppLoadingContents = styled.div`
+  text-align: center;
+  display: flex;
+  padding-bottom: 100px;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
 
+  > img{
+    height: 100px;
+    padding: 20px;
+    margin-bottom: 40px;
+  }
 `;
 
 const AppBody = styled.div`
